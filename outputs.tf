1f120c37275d724e251f4ef6656528d572d313e4
Rod Stewart <rodstewart@gmail.com>
2023-10-05 17:01:44 -0500

    Adds sensitive flag

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,29 @@
 output "azmonitor_out" {
-  value = { for k, v in module.AzureMonitor : k => v }
+  value      = { for k, v in module.AzureMonitor : k => v }
+  sensitive  = true
   # value = module.AzureMonitor
 }
 
 output "law_out" {
-  value = { for k, v in module.AzureMonitor : k => v.law_out }
+  value      = { for k, v in module.AzureMonitor : k => v.law_out }
+  sensitive  = true
   # value = module.AzureMonitor.law_out
 }
 
 output "sa_out" {
-  value = { for k, v in module.AzureMonitor : k => v.sa_out }
+  value      = { for k, v in module.AzureMonitor : k => v.sa_out }
+  sensitive  = true
   # value = module.AzureMonitor.sa_out
 }
 
 output "ehnamespace_out" {
-  value = { for k, v in module.AzureMonitor : k => v.ehnamespace_out }
+  value      = { for k, v in module.AzureMonitor : k => v.ehnamespace_out }
+  sensitive  = true
   # value = module.AzureMonitor.ehnamespace_out
 }
 
 output "eh_out" {
-  value = { for k, v in module.AzureMonitor : k => v.eh_out }
-  # value = module.AzureMonitor.eh_out
+  value     = { for k, v in module.AzureMonitor : k => v.eh_out }
+  sensitive = true
+  # value     = module.AzureMonitor.eh_out
 }
